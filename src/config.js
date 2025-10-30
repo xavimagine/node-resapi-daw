ec59e757664a727071ebff8914ab39a044ef5f31
Javier <62181106+xavimagine@users.noreply.github.com>
2025-10-30 12:07:19 +0100

    Fix typo in environment variable for PORT

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 // procces es un elemento global de node y env almacena todas las variables del ordenador
 // si no existe el puerto pone por defecto el puerto 3000
-export const PORT = process.env.PROT || 3000;
+export const PORT = process.env.PORT || 3000;
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "mypassword";
 export const DB_HOST = process.env.DB_HOST || "localhost";
